266ef7329eef96d52331b0580565836abbe5d5df
mattdesl <dave.des@gmail.com>
2016-05-09 16:38:46 -0400

    more randomness

--- config.js
+++ config.js
@@ -1,30 +1,43 @@
-var SEED = 'i';
+var SEED = String(Math.floor(Math.random() * 1000));
+console.log('Seed:', SEED);
 
 var seedRandom = require('seed-random');
 var palettes = require('./lib/color-palettes');
-var random = typeof SEED === 'undefined'
+var randomFunc = typeof SEED === 'undefined'
   ? Math.random
   : seedRandom(SEED);
 
+var random = require('./lib/random-range')(randomFunc);
+
+var maps = [
+  'maps/church2.jpg',
+  'maps/city4.jpg',
+  'maps/city5.jpg',
+  'maps/eye.jpg'
+];
+
+var mapSrc = maps[Math.floor(random(maps.length))];
+console.log(mapSrc);
+
 module.exports = {
   // rendering options
-  random: random,
-  pointilism: 0,
+  random: randomFunc,
+  pointilism: random(0, 0.1),
   noiseScalar: [ 0.0000001, 0.0002 ],
   globalAlpha: 0.5,
-  startArea: 0.05,
-  maxRadius: 30,
+  startArea: random(0.05, 0.75),
+  maxRadius: random(10, 50),
   lineStyle: 'square',
   interval: 0.01,
-  count: 500,
-  steps: 500,
+  count: Math.floor(random(50, 500)),
+  steps: 1000,
   endlessBrowser: false, // Whether to endlessly step in browser
 
   // background image that drives the algorithm
   debugLuma: false,
   backgroundScale: 1,
   backgorundFille: 'black',
-  backgroundSrc: 'maps/church2.jpg',
+  backgroundSrc: mapSrc,
 
   // browser/node options
   pixelRatio: 1,
