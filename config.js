5a079d3aae7aab6f97fcaaa060a67d34bb7608e0
Sandoche Adittane <sandoche@protonmail.com>
2020-05-17 18:38:03 +0100

    :wrench: Radius and count values changed

--- config.js
+++ config.js
@@ -34,10 +34,10 @@ module.exports = function (seed) {
     noiseScalar: [ random(0.000001, 0.000001), random(0.0002, 0.004) ],
     globalAlpha: 0.5,
     startArea: random(0.0, 1.5),
-    maxRadius: random(5, 100),
+    maxRadius: random(5, 250),
     lineStyle: random(1) > 0.5 ? 'round' : 'square',
     interval: random(0.001, 0.01),
-    count: Math.floor(random(50, 2000)),
+    count: Math.floor(random(50, 10000)),
     steps: Math.floor(random(100, 10000)),
     endlessBrowser: false, // Whether to endlessly step in browser
 
